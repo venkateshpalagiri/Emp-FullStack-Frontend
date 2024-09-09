37191c869deed9ac85a189260456f50be2981d58
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-09-08 23:52:49 -0400

    Routes configured

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
+import FooterComponent from './Components/FooterComponent'
+import HeaderComponent from './Components/HeaderComponent'
 import ListEmployeeComponent from './Components/ListEmployeeComponent'
-import HelloWorld from './HelloWorld'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
 
 function App() {
 
   return (
     <>
-      <ListEmployeeComponent />
+    <BrowserRouter>
+      <HeaderComponent />
+        <Routes>
+          {/* // http://localhost:3000 */}
+          <Route path='/' element={<ListEmployeeComponent />}></Route>
+          {/* // http://localhost:3000/employees */}
+          <Route path='/employees' element={<ListEmployeeComponent />}></Route>
+        </Routes>
+      <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
---
+++ ems-frontend/src/Components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className='footer'>
+        <span>All rights reserved 2024 by Venkatesh </span>
+
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
