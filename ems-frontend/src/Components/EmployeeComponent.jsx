2fe4e7b83751500374b0f10d41c05d5604dbedb8
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-09-09 11:42:17 -0400

    Adding Update Button, Title and Route

--- ems-frontend/src/Components/EmployeeComponent.jsx
+++ ems-frontend/src/Components/EmployeeComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { createEmployee } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
     const [firstName, setFirstName]=useState('')
@@ -8,6 +8,8 @@ const EmployeeComponent = () => {
     const [email,setEmail]=useState('')
 
     const navigator = useNavigate();
+
+    const {id}=useParams();
 
     const [errors, setErrors]=useState({
         firstName:'',
@@ -71,12 +73,22 @@ const EmployeeComponent = () => {
         return valid;
     }
 
+    function pageTitle(){
+        if(id){
+            return <h2 className='text-center'>Update Employee</h2>
+        }else{
+            return <h2 className='text-center'>Add Employee</h2>
+        }
+    }
+
   return (
     <div className='container'>
         <br></br>
         <div className='row'>
             <div className='card col-md-6 offset-md-3 offset-mid-3'>
-                <h2 className='text-center'>Add Employee</h2>
+                {
+                    pageTitle()
+                }
                 <div className='card-body'>
                     <form>
 
--- ems-frontend/src/Components/ListEmployeeComponent.jsx
+++ ems-frontend/src/Components/ListEmployeeComponent.jsx
@@ -21,6 +21,10 @@ const ListEmployeeComponent = () => {
         navigator('/add-employee')
     }
 
+    function updateEmployee(id){
+        navigator(`/edit-employee/${id}`)
+    }
+
   return (
     <div className='container'>
       <h2 className='text-center'>List of Employees</h2>
@@ -32,6 +36,7 @@ const ListEmployeeComponent = () => {
                 <th>Employee First Name</th>
                 <th>Employee Last Name</th>
                 <th>Employee Email Id</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
@@ -42,6 +47,9 @@ const ListEmployeeComponent = () => {
                         <td>{employee.firstName}</td>
                         <td>{employee.lastName}</td>
                         <td>{employee.email}</td>
+                        <td>
+                            <button className='btn btn-info' onClick={()=>updateEmployee(employee.id)}>Update</button>
+                        </td>
 
                     </tr>
                 )
